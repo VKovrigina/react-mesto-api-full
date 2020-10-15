4c21eea4f3bee272473743b840bfe2581890d7a0
VKovrigina <v.kovrigina.2001@yandex.ru>
2020-10-15 12:42:12 +0300

    feat: add auth middlewares

---
+++ middlewares/auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+// eslint-disable-next-line consistent-return
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization && !authorization.startsWith('Bearer ')) {
+    return res
+      .status(401)
+      .send({ message: 'Необходима авторизация' });
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, 'bac35ae5904e7869fb863a4a17e1771bb436cf74b141d7bef6d2cbbb7849a689');
+  } catch (err) {
+    return res
+      .status(401)
+      .send({ message: 'Необходима авторизация' });
+  }
+
+  req.user = payload; // записываем пейлоуд в объект запроса
+
+  next(); // пропускаем запрос дальше
+};
