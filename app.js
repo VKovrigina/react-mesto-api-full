b68481f43204339043615a835a31b5e63bc59c67
VKovrigina <v.kovrigina.2001@yandex.ru>
2020-10-21 12:34:04 +0300

    feat: add cors

--- app.js
+++ app.js
@@ -15,11 +15,25 @@ const auth = require('./middlewares/auth');
 const { validateCreateUser, validateLogin } = require('./middlewares/requestValidation');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
+const allowedCors = [
+  'localhost:3000',
+];
+
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
+});
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  next();
 });
 
 const limiter = rateLimit({
